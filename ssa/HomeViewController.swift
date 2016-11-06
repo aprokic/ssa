db63b72089ad074879345d10b4d10b5b36fe7f69
Muhammad Jahangir <mjahangi@umich.edu>
2016-11-06 16:38:16 -0500

    UI update done.

--- ssa/HomeViewController.swift
+++ ssa/HomeViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController,  UgiInventoryDelegate {
 
     @IBOutlet weak var start_scanning: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let inventory: UgiInventory? = Ugi.singleton().activeInventory
+        if (inventory != nil){
+            inventory!.stop {
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
