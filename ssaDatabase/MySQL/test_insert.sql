3d05806e200f4d5a85546c2875513bbe5dd60a1b
Nils Strand <Nils Strand>
2016-11-20 15:52:01 -0500

    sql inserts to remote db

--- ssaDatabase/MySQL/test_insert.sql
+++ ssaDatabase/MySQL/test_insert.sql
@@ -1,12 +1,12 @@
 insert into locations
-values('000000001', '539 Packard Street, Apartment 1', 'Ann Arbor', 'Michigan', '48104', 'United States');
+values('100000000', '539 Packard Street, Apartment 1', 'Ann Arbor', 'Michigan', '48104', 'United States');
 
 insert into descriptions
-values('000000001', '00000002', 'Living room table', 123.45);
+values('100000000', '20000000', 'Living room table', 123.45);
 insert into descriptions
-values('000000001', '00000003', 'Living room', null);
+values('100000000', '30000000', 'Living room', null);
 
 insert into tags
-values('01', '000000001', '00000002', '0000a');
+values('10', '100000000', '20000000', 'a0000');
 insert into tags
-values('00', '000000001', '00000003', '0000a');
+values('00', '100000000', '30000000', 'a0000');
